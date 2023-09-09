f18d0d6eec5f00fa0bb8aa8108df08c148971754
mrhungerford <55003336+mrhungerford@users.noreply.github.com>
2023-09-09 17:21:03 -0500

    Update uil_app.py

--- uil_app.py
+++ uil_app.py
@@ -1,1 +1,17 @@
+import streamlit as st
 
+from streamlit_gallery import apps, components
+from streamlit_gallery.utils.page import page_group
+
+def main():
+    page = page_group("p")
+
+    with st.sidebar:
+        st.title("UIL APP")
+
+    page.item("Ace editor", components.ace_editor)
+    page.show()
+
+if __name__ == "__main__":
+    st.set_page_config(page_title="Streamlit Gallery by Okld", page_icon="🎈", layout="wide")
+    main()
