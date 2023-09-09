03312f3cf20cb57e8d89ec578696fe0540dd6fd8
mrhungerford <55003336+mrhungerford@users.noreply.github.com>
2023-09-09 17:25:13 -0500

    Update uil_app.py

--- uil_app.py
+++ uil_app.py
@@ -4,10 +4,6 @@ from streamlit_gallery import apps, components
 from streamlit_gallery.utils.page import page_group
 
 def main():
-    page = page_group("p")
-    
-    #page.item("Ace editor", components.ace_editor)
-    page.show()
     components.ace_editor
 
 if __name__ == "__main__":
