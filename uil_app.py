173fb36ca4cb2aca915f57de7c631c32f69d0253
mrhungerford <55003336+mrhungerford@users.noreply.github.com>
2023-09-09 17:38:19 -0500

    Update uil_app.py

--- uil_app.py
+++ uil_app.py
@@ -1,17 +1,15 @@
 import streamlit as st
 
 from streamlit_gallery import apps, components
-from streamlit_gallery.utils.page import page_group
+#from streamlit_gallery.utils.page import page_group
 
 tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
 
 with tab1:
    st.header("A cat")
-   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
 
 with tab2:
    st.header("A dog")
-   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
 
 with tab3:
    st.header("An owl")
