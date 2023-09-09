c1f98c134876512588ad1ad1edacccc9eb3e733b
mrhungerford <55003336+mrhungerford@users.noreply.github.com>
2023-09-09 17:45:08 -0500

    Update uil_app.py

--- uil_app.py
+++ uil_app.py
@@ -1,11 +1,15 @@
 import streamlit as st
 import numpy as np
+import pandas as pd
 
-tab1, tab2 = st.tabs(["📈 Chart", "🗃 Data"])
-data = np.random.randn(10, 1)
+tab1, tab2 = st.tabs(["Leaderboard", "Code Editor"])
 
-tab1.subheader("A tab with a chart")
-tab1.line_chart(data)
+tab1.subheader("A place to see where you stand")
+df = pd.DataFrame(
+   np.random.randn(10, 5),
+   columns=('col %d' % i for i in range(5)))
+tab1.table(df)
 
-tab2.subheader("A tab with the data")
+
+tab2.subheader("A place to write code")
 tab2.write(data)
